cca417b8af3432f85d35de5305707db3a486a81d
Vietor Liu <vietor.liu@gmail.com>
2018-12-27 12:08:27 +0800

    Upgrade version to 1.2.0

--- kwfilter.js
+++ kwfilter.js
@@ -8,22 +8,26 @@ function tryRequire(module) {
     }
 }
 
-function tryLoadModule(relative, name) {
-    var node = tryRequire(relative + name);
+function tryLoadModule(relative, name, suffix) {
+    var node = tryRequire(relative + name + '-' + suffix + '-' + process.arch);
     if (!node)
-        node = tryRequire(relative + '/' + name + '-' + process.arch);
+        node = tryRequire(relative + name + '-' + suffix);
+    if (!node)
+        node = tryRequire(relative + suffix + '-' + process.arch);
+    if (!node)
+        node = tryRequire(relative + suffix);
 }
 
 var kwfilter = tryRequire('./build/Release/kwfilter');
 if (!kwfilter)
-    kwfilter = tryLoadModule('./build/' + process.platform, 'kwfilter');
+    kwfilter = tryLoadModule('./build/', 'kwfilter', process.platform + '-' + process.version);
 if (!kwfilter)
-    kwfilter = tryLoadModule('./build/' + process.platform + '/' + process.version, 'kwfilter');
+    kwfilter = tryLoadModule('./build/', 'kwfilter', process.platform + '-' + (process.version.split('.').slice(0, 2).join('.')));
 if (!kwfilter)
-    kwfilter = tryLoadModule('./build/' + process.platform + '/' + (process.version.split('.').slice(0, 2).join('.')), 'kwfilter');
+    kwfilter = tryLoadModule('./build/', 'kwfilter', process.platform);
 
 if (!kwfilter)
-    throw new Error("not found module for " + process.platform + '(' + process.arch + ')');
+    throw new Error("not found module for " + process.platform + '-' + process.version + '-' + process.arch);
 
 exports.newInstance = function(keywords, mode) {
     return kwfilter.KeywordFilter(keywords, mode || 0);
